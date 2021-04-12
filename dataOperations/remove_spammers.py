a2ff779eeb09d9922174cfbcaf7b83cc96567116
Ryan Kemmer <ryankemmer77@gmail.com>
2021-04-11 21:18:13 -0700

    updated remove spammers

--- dataOperations/remove_spammers.py
+++ dataOperations/remove_spammers.py
@@ -34,19 +34,19 @@ for user in usersCol.find():
 
     for item in responses:
         #check for batch and frame
-        batch = int(item['batch'])
-        frame = int(item['frames'])
-        ran = item['rankings']
-        rat = item['ratings']  
+        if responses['type'] == 'rating':
+            batch = int(item['batch'])
+            frame = int(item['frames'])
+            rat = item['ratings']  
 
-        # For A Test     
-        #if any(x < 10 for x in rat) or any(x>170.05 for x in rat) or np.std(rat) > 36.82:
-        #    print(rat, rat)
+            # For A Test     
+            #if any(x < 10 for x in rat) or any(x>170.05 for x in rat) or np.std(rat) > 36.82:
+            #    print(rat, rat)
 
-        # For B Test     
-        if any(x < 2 for x in rat) or any(x>300 for x in rat) or np.std(rat) > 100.08:
-            print(rat, rat)
-            bad = True
+            # For B Test     
+            if any(x < 2 for x in rat) or any(x>300 for x in rat) or np.std(rat) > 100.08:
+                print(rat)
+                bad = True
 
     if bad == True:
         usersRemove.append(userName)
