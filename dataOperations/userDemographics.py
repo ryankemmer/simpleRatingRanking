062a925a9a9a84ea80a53c28811c8355f00edb74
Ryan Kemmer <ryankemmer77@gmail.com>
2021-04-11 20:56:30 -0700

    added demographic pull

---
+++ dataOperations/userDemographics.py
@@ -0,0 +1,27 @@
+import pymongo
+import json
+import sys
+import numpy as np
+import datetime
+
+url = 'mongodb://localhost:27017/'  
+
+dbase = sys.argv[1]
+print(dbase)
+
+client = pymongo.MongoClient(url)
+db = client[dbase]
+usersCol = db['users']
+responsesCol = db['responses']
+batchesCol = db['batches']
+
+usersArray = []
+
+for user in usersCol.find():
+    usersArray.append(user)
+
+rightNow = datetime.datetime.today().strftime('%m-%d-%Y')
+file_name = 'DEMOGRAPHICS' + rightNow + dbase + ".json" 
+
+with open(str(file_name), 'w+') as outfile:
+	json.dump(usersArray, outfile) 
